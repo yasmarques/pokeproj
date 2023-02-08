bd02ad99c91616927eb2c0ac95bab5d648408f3b
yasmarques <yasmin.marquestito14@gmail.com>
2023-02-07 21:24:17 -0300

    adapter

--- app/src/main/java/br/com/etecia/pokeproj/MainActivity.java
+++ app/src/main/java/br/com/etecia/pokeproj/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.pokeproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.list);
+        MyAdapter adapter = new MyAdapter();
+        list.setAdapter(adapter);
+    }
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
